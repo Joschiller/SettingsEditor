e0564b28426cc8fd05f1782b02d1b817c44800da
Joachim Schiller <83116042+Joschiller@users.noreply.github.com>
2022-11-06 14:31:22 +0100

    feat: implement SettingsEditor

---
+++ SettingsEditor/SettingsEditor/SettingsEditor.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace SettingsEditor
+{
+    /// <summary>
+    /// Renders all <see cref="UserControl"/>s that implement the <see cref="SettingsEditorItem"/> interface.
+    /// <br/>
+    /// The <see cref="SettingsEditorItem"/>s can be grouped by using the <see cref="SettingsEditorItem.GetTabName"/>.
+    /// <br/>
+    /// The <see cref="SettingsEditor"/> offers methods for the common loading, validation and saving of settings.
+    /// </summary>
+    public partial class SettingsEditor : UserControl
+    {
+        /// <summary>
+        /// Currently rendered <see cref="SettingsEditorItem"/>s.
+        /// </summary>
+        private List<SettingsEditorItem> SettingsEditorItems = new List<SettingsEditorItem>();
+
+        public SettingsEditor()
+        {
+            InitializeComponent();
+            InstantiateAllElements();
+        }
+
+        private void InstantiateAllElements()
+        {
+            var controlTypes = AppDomain
+                .CurrentDomain
+                .GetAssemblies()
+                .SelectMany(a => a.GetTypes())
+                .Where(t => typeof(UserControl).IsAssignableFrom(t) && typeof(SettingsEditorItem).IsAssignableFrom(t))
+                .ToList();
+
+            // instantiate
+            foreach (var t in controlTypes)
+            {
+                var control = Activator.CreateInstance(t);
+                var userControl = (UserControl)control;
+                userControl.HorizontalAlignment = HorizontalAlignment.Stretch;
+                userControl.VerticalAlignment = VerticalAlignment.Center;
+                if (((SettingsEditorItem)control).IsVisible()) SettingsEditorItems.Add((SettingsEditorItem)control);
+            }
+
+            // render on tabs
+            foreach (var tabName in SettingsEditorItems.Select(item => item.GetTabName()).Distinct())
+            {
+                var stackPanel = new StackPanel();
+                stackPanel.Orientation = Orientation.Vertical;
+                SettingsEditorItems
+                    .Where(item => item.GetTabName() == tabName)
+                    .OrderBy(item => item.GetControlName())
+                    .ToList()
+                    .ForEach(item => stackPanel.Children.Add((UserControl)item));
+
+                var scrollViewer = new ScrollViewer();
+                scrollViewer.VerticalScrollBarVisibility = ScrollBarVisibility.Auto;
+                scrollViewer.HorizontalScrollBarVisibility = ScrollBarVisibility.Disabled;
+                scrollViewer.Content = stackPanel;
+
+                var tabitem = new TabItem();
+                tabitem.Header = tabName;
+                tabitem.Content = scrollViewer;
+
+                settingsTabs.Items.Add(tabitem);
+            }
+        }
+
+        /// <summary>
+        /// Load the current settings into all rendered <see cref="SettingsEditorItem"/>s.
+        /// </summary>
+        /// <param name="accountIdentifier">Identifier of the current account</param>
+        public void LoadData(int? identification)
+        {
+            SettingsEditorItems.ForEach(item => item.LoadData(identification));
+        }
+        /// <summary>
+        /// Validate the current settings of all rendered <see cref="SettingsEditorItem"/>s.
+        /// </summary>
+        /// <returns>Whether all currently configured settings are valid.</returns>
+        public bool ValidateData()
+        {
+            return SettingsEditorItems.All(item => item.ValidateData());
+        }
+        /// <summary>
+        /// Save the current settings of all rendered <see cref="SettingsEditorItem"/>s.
+        /// <br/>
+        /// Before saving, the value should be validated with <see cref="ValidateData"/>.
+        /// </summary>
+        public void SaveData()
+        {
+            SettingsEditorItems.ForEach(item => item.SaveData());
+        }
+    }
+}
---
+++ SettingsEditor/SettingsEditor/SettingsEditorItem.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows.Controls;
+
+namespace SettingsEditor
+{
+    /// <summary>
+    /// An item that will be rendered within the <see cref="SettingsEditor"/>.
+    /// <br/>
+    /// Implement this interface within any <see cref="UserControl"/> to let the <see cref="UserControl"/> be rendered within any generated instance of the <see cref="SettingsEditor"/>.
+    /// </summary>
+    public interface SettingsEditorItem
+    {
+        #region Data
+        /// <summary>
+        /// Load the settings data into the control.
+        /// </summary>
+        /// <param name="accountIdentifier">Identifier of the current account</param>
+        void LoadData(int? accountIdentifier);
+        /// <summary>
+        /// Validate the current settings value.
+        /// </summary>
+        /// <returns>Whether the current value is valid</returns>
+        bool ValidateData();
+        /// <summary>
+        /// Save the current settings value.
+        /// <br/>
+        /// Before saving, the value should be validated with <see cref="ValidateData"/> or via <see cref="SettingsEditor.ValidateData"/> of the containing <see cref="SettingsEditor"/>.
+        /// </summary>
+        void SaveData();
+        #endregion
+        #region Rendering
+        /// <returns>The tab the <see cref="SettingsEditorItem"/> should be rendered within the <see cref="SettingsEditor"/>.</returns>
+        string GetTabName();
+        /// <returns>
+        /// The control name of the <see cref="SettingsEditorItem"/>.
+        /// <br/>
+        /// This property can be used to sort the controls within the <see cref="SettingsEditor"/>.
+        /// </returns>
+        string GetControlName();
+        /// <returns>Whether the <see cref="SettingsEditorItem"/> should be rendered.</returns>
+        bool IsVisible();
+        #endregion
+    }
+}
